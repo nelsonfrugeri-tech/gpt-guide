f7c78bbd2b6979eabedd40a1283f5ec5dde700d0
Nelson Frugeri Jr <nfrugeri@gmail.com>
2023-04-09 11:17:33 -0300

    Improve it

--- parameters_on_prompt/pprompt.py
+++ parameters_on_prompt/pprompt.py
@@ -2,34 +2,38 @@ import openai
 from dotenv import load_dotenv
 import os
 
-def generate_completion(prompt, max_tokens):
-    # Define the model and parameters to use for the completion
-    engine = "text-davinci-003"
+# Load environment variables from .env file
+load_dotenv()
+
+# Set OpenAI API key from environment variable
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# Define the model and parameters to use for the completion
+engine = "text-davinci-003"
+
+def generate_completion(params):
 
     # Use the OpenAI API to generate a completion
     response = openai.Completion.create(
         engine=engine,
-        prompt=prompt,
-        max_tokens=max_tokens
+        prompt=params.get("prompt", "How can you help me?"),
+        max_tokens=params.get("max_tokens", 50)
     )
 
     return response.choices[0].text.strip()
 
 def main():
-    # Load environment variables from .env file
-    load_dotenv()
 
-    # Set OpenAI API key from environment variable
-    openai.api_key = os.getenv("OPENAI_API_KEY")
+    params = {}
 
     # Define the prompt that the model will complete
-    prompt = input("Enter a prompt: ")
+    params["prompt"] = input("Enter a prompt: ")
 
     # Define the number max of the tokens
-    max_tokens = int(input("Enter the max tokens: "))
+    params["max_tokens"] = int(input("Enter the max tokens: "))
 
     # Print the completion text
-    print(generate_completion(prompt, max_tokens))
+    print(generate_completion(params))
 
 if __name__ == "__main__":
     main()
