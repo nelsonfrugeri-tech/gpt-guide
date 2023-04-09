8bc8f26735b5409e1447062530d9062c566c432b
Nelson Frugeri Jr <nfrugeri@gmail.com>
2023-04-09 11:47:52 -0300

    Refactor to the improvement

--- parameters_on_prompt/pprompt.py
+++ parameters_on_prompt/pprompt.py
@@ -2,38 +2,23 @@ import openai
 from dotenv import load_dotenv
 import os
 
-# Load environment variables from .env file
-load_dotenv()
-
-# Set OpenAI API key from environment variable
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# Set the model to use for the completion
-engine = os.getenv("OPENAI_MODEL")
-
 def generate_completion(params):
-
-    # Use the OpenAI API to generate a completion
-    response = openai.Completion.create(
-        engine=engine,
+    return openai.Completion.create(
+        engine=os.getenv("OPENAI_MODEL"),
         prompt=params.get("prompt", "How can you help me?"),
         max_tokens=params.get("max_tokens", 50)
-    )
+    ).choices[0].text.strip()
 
-    return response.choices[0].text.strip()
+def prompt():
+    return {
+        "prompt": input("Enter a prompt: "),
+        "max_tokens": int(input("Enter the max tokens: "))
+    }
 
 def main():
-
-    params = {}
-
-    # Define the prompt that the model will complete
-    params["prompt"] = input("Enter a prompt: ")
-
-    # Define the number max of the tokens
-    params["max_tokens"] = int(input("Enter the max tokens: "))
-
-    # Print the completion text
-    print(generate_completion(params))
+    print(generate_completion(prompt()))
 
 if __name__ == "__main__":
     main()
