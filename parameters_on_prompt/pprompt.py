5694b353f10ecd6f8181dcbd02b467ac47635afe
Nelson Frugeri Jr <nfrugeri@gmail.com>
2023-04-09 11:03:34 -0300

    Implemented the max_tokens parameters

--- parameters_on_prompt/pprompt.py
+++ parameters_on_prompt/pprompt.py
@@ -11,13 +11,17 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 # Define the prompt that the model will complete
 prompt = input("Enter a prompt: ")
 
+# Define the number max of the tokens
+max_tokens = int(input("Enter the max tokens: "))
+
 # Define the model and parameters to use for the completion
-model_engine = "text-davinci-003"
+engine = "text-davinci-003"
 
 # Use the OpenAI API to generate a completion
 response = openai.Completion.create(
-    engine=model_engine,
+    engine=engine,
     prompt=prompt,
+    max_tokens=max_tokens
 )
 
 # Print the completion text
