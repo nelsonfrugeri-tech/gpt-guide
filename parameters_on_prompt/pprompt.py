7a3e95d6b34176bdd24c05e365f065cb36c3a8e2
Nelson Frugeri Jr <nfrugeri@gmail.com>
2023-04-10 16:32:32 -0300

    Included parameters on the prompt

--- parameters_on_prompt/pprompt.py
+++ parameters_on_prompt/pprompt.py
@@ -13,8 +13,14 @@ def generate_completion(params):
 def prompt():
     return {
         "prompt": input("Enter a prompt: "),
-        "max_tokens": int(input("Enter the max tokens: ")),
-        "temperature": int(input("Enter the creativity value of the text, from 0 to 1, 0 for least creative and 1 for most creative: "))
+        "max_tokens": int(input("Enter the max tokens: ") or 50),
+        "temperature": int(input("Enter the creativity value of the text, from 0 to 1, 0 for least creative and 1 for most creative: ") or 0.5),
+        "top_p": int(input("Enter the imagination value of the text, from 0 to 1, 0 for least imaginative and 1 for most imaginative: ") or 1),
+        "n": int(input("How many answers would you like to receive?: ") or 1),
+        "stop": input("If you want to stop the answer at any point include the text here?: ") or "",
+        "best_of": input("Among the answers, if there are more than one, how many better answers do you want?: ") or 1,
+        "suffix": input("Add words or text to add more context to your prompt: ") or "",
+        "echo": input("Do you want to return the prompt entry in the answer?: ") or True
     }
 
 def main():
