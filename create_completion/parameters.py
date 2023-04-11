2f8e019b89c5cb05695f417bb1b50641ab7d0aab
Nelson Frugeri Jr <nfrugeri@gmail.com>
2023-04-11 09:30:41 -0300

    Improvement create completion code

--- create_completion/parameters.py
+++ create_completion/parameters.py
@@ -1,3 +1,4 @@
+from decimal import ROUND_HALF_UP, Decimal
 import openai
 import os
 
@@ -11,8 +12,10 @@ def prompt():
         "engine": os.getenv("OPENAI_MODEL"),
         "prompt": input("Enter a prompt: "),
         "max_tokens": int(input("Enter the max tokens: ") or 50),
-        "temperature": int(input("Enter the creativity value of the text, from 0 to 1, 0 for least creative and 1 for most creative: ") or 0.5),
-        "top_p": int(input("Enter the imagination value of the text, from 0 to 1, 0 for least imaginative and 1 for most imaginative: ") or 1),
+        "temperature": float(Decimal(input("Enter the creativity value of the text, from 0 to 1, 0 for least creative and 1 for most creative: ") or 0.5)
+            .quantize(Decimal('0.1'), rounding=ROUND_HALF_UP)),
+        "top_p": float(Decimal(input("Enter the imagination value of the text, from 0 to 1, 0 for least imaginative and 1 for most imaginative: ") or 1)
+            .quantize(Decimal('0.1'), rounding=ROUND_HALF_UP)),
         "n": int(input("How many answers would you like to receive?: ") or 1),
         "stop": input("If you want to stop the answer at any point include the text here?: ") or "",
         "best_of": int(input("Among the answers, if there are more than one, how many better answers do you want?: ") or 1),
@@ -21,7 +24,8 @@ def prompt():
     }
 
 def main():
-    print(generate_completion(prompt()))
+    for choice in generate_completion(prompt())["choices"]:
+        print(choice["text"])
 
 if __name__ == "__main__":
     main()
